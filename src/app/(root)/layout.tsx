2b3788491e7ab84c0342ae0939ef4c79e5fae5cc
Aman Gupta <amangupta954055@gmail.com>
2024-03-02 01:46:22 +0530

    add lenis smooth scroll

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,15 +1,28 @@
-import Navbar from '@/components/Navbar'
-import React from 'react'
+'use client'
+import Navbar from "@/components/Navbar";
+import Lenis from '@studio-freight/lenis'
+import React, { useEffect, useLayoutEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const RootLayout = ({children} : {children : React.ReactNode}) => {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger)
+    const lenis = new Lenis();
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0);
+  }, []);
   return (
-    <div className='h-full bg-cream '>
-      <main>
-      
-        {children}
-      </main>
+    <div className="h-full bg-cream ">
+      <main>{children}</main>
     </div>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
